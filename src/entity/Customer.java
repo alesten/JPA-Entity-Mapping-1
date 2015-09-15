b6d952ac4fa68c50d1589a5262cd0d6b2d0eaee9
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-09-15 13:00:27 +0200

    Added list of hobbies to customer

--- src/entity/Customer.java
+++ src/entity/Customer.java
@@ -1,18 +1,29 @@
 package entity;
 
+import enums.CustomerType;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @Enumerated(EnumType.STRING)
+    private CustomerType customerType;
+    @ElementCollection()
+    private List<String> hobbies = new ArrayList();
 
     public Customer() {
     }
@@ -21,12 +32,9 @@ public class Customer implements Serializable {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Customer(String name, CustomerType customerType) {
         this.name = name;
+        this.customerType = customerType;
     }
 
     public Long getId() {
@@ -37,6 +45,42 @@ public class Customer implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+//    public String getHobbies() {
+//        String s = "";
+//        for (String hobby : hobbies) {
+//            s += hobby + ",";
+//        }
+//        return s.substring(0, s.length() -1);
+//    }
+    
+    public void addHobbby(String s){
+        hobbies.add(s);
+    }
+    
     @Override
     public int hashCode() {
         int hash = 0;
--- src/test/Tester.java
+++ src/test/Tester.java
@@ -1,6 +1,7 @@
 package test;
 
 import entity.Customer;
+import enums.CustomerType;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -9,7 +10,10 @@ public class Tester {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_Entity_Mapping_1PU");
         EntityManager em = emf.createEntityManager();
         
-        Customer c1 = new Customer("Ole");
+        Customer c1 = new Customer("Ole Erik", CustomerType.GOLD);
+        c1.addHobbby("Håndbold");
+        c1.addHobbby("Fodbold");
+        c1.addHobbby("PC");
         
         em.getTransaction().begin();
         
@@ -18,3 +22,16 @@ public class Tester {
         em.getTransaction().commit();
     }
 }
+
+
+/*
+    4. Tablet bliver udfyldt med data der bliver indtastet i addcustomers.sql 
+    før den kalder min egen kode. Dette resultere i at min kode ikke kan tilføjes 
+    da ID 1 allerede er brugt.
+
+    5.1. Der kommer Customertype ind i tablets og enumens værdig bliver sat ind.
+    5.2. Nu bliver enumens navn sat ind i stedet.
+
+    6.1. Uden @ElementCollection() indsætter den ulæslig bytedata.
+    6.2. Med @ElementCollection() indsætter den en ny tabel som kæder hobby med customer
+*/
